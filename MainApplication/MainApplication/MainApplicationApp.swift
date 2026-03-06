568786338543acfee914bd21cd2f5091231974ce
Abhishek <chatterjee.abhishek@boeing.com>
2026-03-06 10:07:00 +0530

    Initial commit

---
+++ MainApplication/MainApplication/MainApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainApplicationApp.swift
+//  MainApplication
+//
+//  Created by Abhishek on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MainApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
